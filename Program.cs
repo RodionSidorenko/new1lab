63ca2c614e2613cc38f502518df23442c15fc450
RodionSidorenko <mirelego22@gmail.com>
2020-10-28 00:11:23 +0200

    1

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace new_lab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите число х:\n");
+            double x = Convert.ToDouble(Console.ReadLine());
+
+            if (x <-5 && x >-10 & x <= 4.5 && x >= -4.5)
+            {
+                double y = Math.Cos(Math.Pow(x, 2))/(Math.Pow(Math.Sin(2*x), 2)+1);
+                Console.WriteLine(y);
+            }
+
+            else
+            {
+                double y = (Math.Pow(-x, 2) + 3)/(2 - x);
+                Console.WriteLine(y);
+        }
+    }
+    }
+}
